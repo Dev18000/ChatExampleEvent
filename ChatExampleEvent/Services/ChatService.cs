47f5f4e452e5716e9a85ad365d27627ef52c2bef
farida <dev18@laposte.net>
2025-04-08 13:26:32 +0200

    Добавьте файлы проекта.

---
+++ ChatExampleEvent/Services/ChatService.cs
@@ -0,0 +1,12 @@
+﻿namespace ChatExampleEvent.Services
+{
+    public class ChatService
+    {
+        public event Action<string, string>? OnMessageReceived;
+
+        public void SendMessage(string user, string message)
+        {
+            OnMessageReceived?.Invoke(user, message);
+        }
+    }
+}
