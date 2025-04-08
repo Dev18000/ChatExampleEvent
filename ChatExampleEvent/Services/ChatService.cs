6b991aae90b5681029f30c759285aa059883354a
farida <dev18@laposte.net>
2025-04-08 14:03:49 +0200

    test stress

--- ChatExampleEvent/Services/ChatService.cs
+++ ChatExampleEvent/Services/ChatService.cs
@@ -2,31 +2,37 @@
 {
     public class ChatService
     {
-        private readonly object _lock = new();
+        //private readonly object _lock = new();
 
-        private event Action<string, string>? _onMessageReceived;
+        //private event Action<string, string>? _onMessageReceived;
 
-        public event Action<string, string> OnMessageReceived
-        {
-            add
-            {
-                lock (_lock)
-                {
-                    _onMessageReceived += value;
-                }
-            }
-            remove
-            {
-                lock (_lock)
-                {
-                    _onMessageReceived -= value;
-                }
-            }
-        }
+        //public event Action<string, string> OnMessageReceived
+        //{
+        //    add
+        //    {
+        //        lock (_lock)
+        //        {
+        //            _onMessageReceived += value;
+        //        }
+        //    }
+        //    remove
+        //    {
+        //        lock (_lock)
+        //        {
+        //            _onMessageReceived -= value;
+        //        }
+        //    }
+        //}
+
+        //public void SendMessage(string user, string message)
+        //{
+        //    _onMessageReceived?.Invoke(user, message);
+        //}
+        public event Action<string, string>? OnMessageReceived;
 
         public void SendMessage(string user, string message)
         {
-            _onMessageReceived?.Invoke(user, message);
+            OnMessageReceived?.Invoke(user, message);
         }
     }
 }
